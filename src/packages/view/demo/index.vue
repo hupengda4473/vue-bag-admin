15c8b3c731212d3facd30ef40b8cb23f1ba5e17e
胡鹏达 <295562365@qq.com>
2023-11-14 11:12:00 +0800

    1.添加less依赖 2.添加demo全屏页

---
+++ src/packages/view/demo/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="content">
+        <n-button strong secondary type="primary" @click="back">
+            返回系统
+        </n-button>
+    </div>
+</template>
+
+<script setup>
+    const back = () => {
+        window.location.href="javascript:history.go(-1)";
+    }
+</script>
+
+<style scoped lang="less">
+.content{
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+</style>
